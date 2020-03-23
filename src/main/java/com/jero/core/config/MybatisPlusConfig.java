9ea211f0f99b8ef59e9136fa6dd6d5a67d8b1cd2
Li Xuetao <lxtalx@gmail.com>
2020-03-23 17:15:35 +0800

    【修改】添加mybatis-plus分页插件

--- src/main/java/com/jero/core/config/MybatisPlusConfig.java
+++ src/main/java/com/jero/core/config/MybatisPlusConfig.java
@@ -2,6 +2,7 @@ package com.jero.core.config;
 
 import com.baomidou.mybatisplus.autoconfigure.MybatisPlusPropertiesCustomizer;
 import com.baomidou.mybatisplus.core.config.GlobalConfig;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.jero.core.config.mybatis.SnowFlakeGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +10,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MybatisPlusConfig {
 
+    /**
+     * ID生成器
+     * @return
+     */
     @Bean
     public MybatisPlusPropertiesCustomizer plusPropertiesCustomizer() {
         return plusProperties -> plusProperties.getGlobalConfig().setIdentifierGenerator(new SnowFlakeGenerator());
+    }
+
+    /**
+     * 分页插件
+     * @return
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        return new PaginationInterceptor();
     }
 
 }
