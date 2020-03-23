dc8bda2f49e5b307134f3989e25046fc2b2d7848
Li Xuetao <lxtalx@gmail.com>
2020-03-23 22:13:52 +0800

    【修改】添加自定义构造条件

---
+++ src/main/java/com/jero/mp/JeroSqlCondition.java
@@ -0,0 +1,14 @@
+package com.jero.mp;
+
+import com.baomidou.mybatisplus.annotation.SqlCondition;
+
+/**
+ * mybatis plus Table Field sql构造条件
+ * @date 2020-03-23
+ */
+public class JeroSqlCondition extends SqlCondition {
+
+    public static final String GT = "%s&gt;#{%s}";
+    public static final String LT = "%s&lt;#{%s}";
+
+}
