09e032b035fc6e928e9349a7e0aadd02f35afa81
zeroBlack <lxtalx@gmail.com>
2020-03-23 11:25:44 +0800

    【修改】添加starter

--- src/main/java/com/jero/core/config/MybatisPlusConfig.java
+++ src/main/java/com/jero/core/config/MybatisPlusConfig.java
@@ -1,7 +1,6 @@
 package com.jero.core.config;
 
 import com.baomidou.mybatisplus.autoconfigure.MybatisPlusPropertiesCustomizer;
-import com.baomidou.mybatisplus.core.config.GlobalConfig;
 import com.jero.core.config.mybatis.SnowFlakeGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
