a53ecc8b22e2eed99664b0161162d627d9111f58
Li Xuetao <lxtalx@gmail.com>
2020-03-25 15:50:52 +0800

    【添加】基础实体 【添加】基础控制器

--- src/main/java/com/jero/core/BaseController.java
+++ src/main/java/com/jero/core/BaseController.java
@@ -1,8 +1,16 @@
 package com.jero.core;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.jero.http.page.PageInfo;
+import com.jero.mp.entity.BaseEntity;
 import org.apache.poi.ss.formula.functions.T;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 基础控制器，包含处理分页等方法
@@ -10,7 +18,33 @@ import org.apache.poi.ss.formula.functions.T;
  * @author lixuetao
  * @date 2020-03-25
  */
-public class BaseController {
+public abstract class BaseController<T extends BaseEntity<?>> {
+
+    public BaseController(){}
+
+    public T getById(Serializable id){
+        return this.getService().getById(id);
+    }
+
+    public List<T> list(T t){
+        return this.getService().list(Wrappers.query(t));
+    }
+
+    public IPage<T> page(Page page, T t){
+        return this.getService().page(page, Wrappers.query(t));
+    }
+
+    public boolean save(T data){
+        return this.getService().save(data);
+    }
+
+    public boolean update(T data){
+        return this.getService().updateById(data);
+    }
+
+    public boolean deleteBatchIds(Serializable... ids){
+        return this.getService().removeByIds(Arrays.asList(ids));
+    }
 
     public PageInfo<T> getPageInfo(IPage<T> page) {
         PageInfo<T> pageInfo = new PageInfo();
@@ -22,5 +56,6 @@ public class BaseController {
         return pageInfo;
     }
 
+    protected abstract IService<T> getService();
 
 }
---
+++ src/main/java/com/jero/mp/entity/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.jero.mp.entity;
+
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+
+/**
+ * @Description 基础实体，提供所有的实体类继承
+ * @Author lixuetao
+ * @Date 2020/3/25
+ **/
+public class BaseEntity<T extends BaseEntity<?>> extends Model<BaseEntity<T>> {
+
+    public BaseEntity(){}
+
+    @Override
+    public String toString(){
+        return "BaseEntity()";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        } else if (!(o instanceof BaseEntity)) {
+            return false;
+        } else {
+            BaseEntity<?> other = (BaseEntity)o;
+            return other instanceof BaseEntity;
+        }
+    }
+
+}
