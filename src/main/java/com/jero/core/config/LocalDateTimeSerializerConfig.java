9e0eac162eff38198dbe549b23476e50e599f428
Li Xuetao <lxtalx@gmail.com>
2020-04-20 23:57:28 +0800

    [添加]添加全局时间转换配置

---
+++ src/main/java/com/jero/core/config/LocalDateTimeSerializerConfig.java
@@ -0,0 +1,32 @@
+package com.jero.core.config;
+
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @Description 本地时间全局转换配置
+ * @Author lixuetao
+ * @Date 2020/4/20
+ **/
+@Configurable
+public class LocalDateTimeSerializerConfig {
+
+    @Value("${spring.jackson.date-format:yyyy-MM-dd HH:mm:ss}")
+    private String pattern;
+
+    @Bean
+    public LocalDateTimeSerializer localDateTimeSerializer(){
+        return new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer(){
+        return builder -> builder.serializerByType(LocalDateTimeSerializerConfig.class, localDateTimeSerializer());
+    }
+
+}
